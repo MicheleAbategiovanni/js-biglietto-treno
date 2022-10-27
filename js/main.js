c47266300472110d0f9c904f98b6ef6282cba442
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-10-27 17:19:40 +0200

    added new var and fix codes

--- js/main.js
+++ js/main.js
@@ -2,26 +2,33 @@ let ticket = parseInt(prompt("Inserisci i km che dovrai percorrere: "));
 
 let eta = parseInt(prompt("Inserisci eta: "));
 
+let biglietto;
+
 console.log(`Il percorso da te indicato è di : ${ticket} km che dovrai percorre`);
 
 console.log(`L'età del passeggero è di : ${eta} anni`);
 
+km.innerHTML = ticket;
 
-ticket = ticket * 0.21;
+biglietto = ticket * 0.21;
 
-console.log(`Il prezzo pieno del biglietto è di : ${ticket.toFixed(2)} euro`);
+console.log(`Il prezzo pieno del biglietto è di : ${biglietto.toFixed(2)} euro`);
 
 
 if (eta < 18) {
 
-    ticket = ticket - (ticket * 0.20);
+    let bigliettoTeen;
 
-    console.log(`Il prezzo del ticket scontato per giovani è: ${ticket.toFixed(2)} euro`);
+    bigliettoTeen = biglietto - (biglietto * 0.20);
+
+    console.log(`Il prezzo del ticket scontato per giovani è: ${bigliettoTeen.toFixed(2)} euro`);
 
 } else if (eta > 65) {
 
-    ticket = ticket - (ticket * 0.40);
+    let bigliettoOver65;
 
-    console.log(`Il prezzo del ticket scontato per over 65 è: ${ticket.toFixed(2)} euro `);
+    bigliettoOver65 = biglietto - (biglietto * 0.40);
+
+    console.log(`Il prezzo del ticket scontato per over 65 è: ${bigliettoOver65.toFixed(2)} euro `);
 
 }
