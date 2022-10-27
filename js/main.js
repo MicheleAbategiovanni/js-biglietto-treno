a5ca5376bc84e7271a24f7b6316240f62c52e1cb
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-10-27 15:19:15 +0200

    complete coding

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let ticket = parseInt(prompt("Inserisci i km che dovrai percorrere: "));
+
+let eta = parseInt(prompt("Inserisci eta: "));
+
+console.log(`Il percorso da te indicato è di : ${ticket} km che dovrai percorre`);
+
+console.log(`L'età del passeggero è di : ${eta} anni`);
+
+ticket = ticket * 0.21;
+
+console.log(`Il prezzo pieno del biglietto è di : ${ticket.toFixed(2)} euro`);
+
+if (eta < 18) {
+
+    ticket = ticket - (ticket * 0.20);
+
+    console.log(`Il prezzo del ticket scontato per giovani è: ${ticket.toFixed(2)} euro`);
+
+} else if (eta > 65) {
+
+    ticket = ticket - (ticket * 0.40);
+
+    console.log(`Il prezzo del ticket scontato per over 65 è: ${ticket.toFixed(2)} euro `);
+
+}
